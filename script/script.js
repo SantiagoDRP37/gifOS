05129f0caacee9228489086aaf88aa7d52393ecb
David Santiago Rojas Páez <santiagodrp37@gmail.com>
2020-03-30 03:14:59 -0500

    utiliza la api para buscar gif

--- script/script.js
+++ script/script.js
@@ -1,13 +1,47 @@
-new Promise((_, reject) => reject(new Error("Fallo")))
+const apiKey = 'M3pHPCucA0Juey10cWpWR1lRfkhJFfzi';
+const q = 'gatos';
+const path = `http://api.giphy.com/v1/gifs/search?api_key=${apiKey}&q=${q}`;
+//funcion mostrar resultados de busqueda
+function mostrarBusqueda(datos){
+  const resultados = document.getElementById('resultados'); 
+  let resultHtml='';
+  resultados.innerHTML = resultHtml; //vacia las etiquetas dentro resultados
+  datos.forEach(element => {
+    let conteGif = document.createElement("div");
+            conteGif.setAttribute("class", "conteGif");
 
-  .then(value => console.log("Handler 1"))
+            conteGif.innerHTML +=
 
-  .catch(reason => {
-
-  console.log("Ocurrió un error:" + reason);
-
-  return "nada";
-
-})
-
-  .then(value => console.log("Handler 2", value));
+                ' <img src =' + element.images.downsized.url + '>' +
+                ' <div class = "foot">' +
+                '  <p>' + element.title + '</p>' +
+                ' </div>';
+                resultados.appendChild(conteGif) ;
+  });
+  
+ 
+  
+}
+//funcion buscar gift de la barra de busqueda
+function buscarGif(){
+  const busq = fetch(path)
+        .then((res) => {
+            //console.log(res.json());
+            return res.json();
+        })
+        .then(data=>{
+          
+          let datos = data.data;
+          mostrarBusqueda(datos);
+          
+          return data;
+        })
+        .catch(error => {
+            return error;
+        });
+  return busq;
+}
+window.onload = () => {
+  
+  buscarGif();
+}
