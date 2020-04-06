29ca16755fc4fff84aa6d31049eb8fb8479f52b4
Portatil_DSRP <santiagodrp37@gmail.com>
2020-04-06 17:21:46 -0500

    maquetado de la pagina de Grabacion

--- script/videoRecord.js
+++ script/videoRecord.js
@@ -1,1 +1,9 @@
-prompt('hola')
+//************************************** eventos *************************************************************//
+//Evento boton Comenzar grabacion
+let letrero =  document.getElementsByClassName("mainCrearGif")[0];
+let botonComenzar = document.getElementById("botonComenzar")
+botonComenzar.addEventListener("click",()=>{
+    letrero.style.cssText = 'display:none;';
+})
+
+//
